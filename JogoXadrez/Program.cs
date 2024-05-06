eee4d4721664b34ee799f6bd0f9781c2c9b80dd2
Frank Williams <ffrank.williams@gmail.com>
2024-05-06 20:59:50 -0300

    Mandando para o GitHub

---
+++ JogoXadrez/Program.cs
@@ -0,0 +1,17 @@
+﻿using tabuleiro;
+
+namespace JogoXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            Tela.ImprimirTabuleiro(tab);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ JogoXadrez/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,29 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas {  get; set; }
+        public int Colunas { get; set; }
+
+        private Peca[,] Pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+
+        }
+
+        public Peca Peca(int linha, int coluna)
+        {
+            return Pecas[linha, coluna];
+        }
+
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            Pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
+        }
+    }
+}
